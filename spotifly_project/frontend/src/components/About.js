6c77950efabef5f09b4803477c00d478a8042b2d
gx31619 <soccermanabc@gmail.com>
2022-03-17 06:40:30 -0400

    temp changes for about us page

--- spotifly_project/frontend/src/components/About.js
+++ spotifly_project/frontend/src/components/About.js
@@ -12,13 +12,13 @@ const About = () => {
       </ScrollPage>
 
       <ScrollPage page={1}>
-        <Animator animation={batch(Sticky(), ZoomIn(10, 6), Move(0, -200))}>
+        <Animator animation={batch(Sticky(), ZoomIn(), Move(0, -200))}>
           <h1 style={{ color: 'blue' }}>What?</h1>
         </Animator>
 
       </ScrollPage>
       <ScrollPage page={2}>
-        <Animator animation={batch(Sticky(), MoveIn(), ZoomIn(10, 5))} >
+        <Animator animation={batch(Sticky(), MoveIn(), ZoomIn())} >
           <p style={{ color: 'blue' }}>spotiFly is a webapp that allows you to connect your Spotify and create/curate playlists based on selected properties in your liked songs, such as how "danceable" a song is, the genre, the BPM, etc.</p>
         </Animator>
       </ScrollPage>
